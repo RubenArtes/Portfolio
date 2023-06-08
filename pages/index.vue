6a3e11a4f63e19bdef5a69f1150c407a926e12df
RubenArtes <rubendi98@gmail.com>
2023-06-08 19:19:33 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app bg-black">
-    <div class="header bg-black p-10 flex text-white justify-between">
+    <div class="header bg-black p-10 flex text-white justify-between sticky top-0">
       <div class="image flex flex-start">
         <img src="/img/Vector.svg">
       </div>
