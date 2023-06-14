d7f5b996d4885b8b0f7932a7e62e845f7fa92f76
RubenArtes <rubendi98@gmail.com>
2023-06-14 13:22:52 +0200

    Update packaging.vue

--- pages/packaging.vue
+++ pages/packaging.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="app bg-[#FEE8D4]">
-        <div class="header bg-[#FFE0C5] p-10 flex text-zinc-600 justify-between sticky top-0 z-10">
+        <div class="header bg-[#FFE0C5] p-10 flex text-zinc-600 justify-between sticky top-0 z-10 max-md:p-6 max-md:items-center">
             <div class="image flex flex-start">
                 <NuxtLink to="/">
-                    <img class="vector" src="/img/Vectorzinc.svg">
+                    <img class="vector max-md:max-w-[15px]" src="/img/Vectorzinc.svg">
                 </NuxtLink>
             </div>
-            <div class="email text-lg flex gap-2 items-center">ruben.maldonado.graphic@gmail.com
+            <div class="email text-lg flex gap-2 items-center max-md:text-xs">ruben.maldonado.graphic@gmail.com
                 <NuxtLink to="/">
-                    <img class="vector" src="/img/Vector-11zinc.svg">
+                    <img class="vector max-md:max-w-[8px]" src="/img/Vector-11zinc.svg">
                 </NuxtLink>
             </div>
         </div>
-        <div class="trabajos flex pt-20 px-40 pb-20 text-zinc-600 flex-nowrap items-center gap-64 justify-center">
-            <div class="titular text-[70px] font-normal max-w-[13ch] leading-12">Packaging</div>
+        <div class="trabajos flex pt-20 px-40 pb-20 text-zinc-600 flex-nowrap items-center gap-64 justify-center max-md:pt-16 max-md:px-10 max-md:flex-col max-md:justify-center max-md:gap-10">
+            <div class="titular text-[70px] font-normal max-w-[13ch] leading-12 max-md:text-[70px] max-md:justify-center">Packaging</div>
             <div class="info flex flex-col gap-6">
-                <div class="text max-w-[60ch] text-[16px] leading-10 text-zinc-400">Diseño Matutano - Packaging - Marca -
+                <div class="text max-w-[60ch] text-[16px] leading-10 text-zinc-400 max-md:leading-2 max-md:text-xs">Diseño Matutano - Packaging - Marca -
                     Diseño Gráfico</div>
-                <div class="text max-w-[60ch] text-[20px] leading-8 text-zinc-600 font-bold">El proyecto consistirá en el
+                <div class="text max-w-[60ch] text-[20px] leading-8 text-zinc-600 font-bold max-md:text-sm max-md:max-w-[40ch]">El proyecto consistirá en el
                     diseño de una gama
                     de productos para una marca existente (Matutano), generando
                     un sistema gráfico aplicable y coherente en, al
@@ -28,18 +28,18 @@
                 </div>
             </div>
         </div>
-        <div class="video flex pt-20 px-64 pb-20 text-zinc-600 flex-nowrap items-center gap-64 justify-center">
+        <div class="video flex pt-20 px-64 pb-20 text-zinc-600 flex-nowrap items-center gap-64 justify-center max-md:pt-2 max-md:pb-2 max-md:px-10 max-md:flex-col max-md:justify-center max-md:gap-10">
             <video width="1240" height="788" loop autoplay controls>
                 <source src="/img/Comp_1_2.mp4" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
 
         </div>
-        <div class="colum flex pt-20 px-64 pb-10 text-zinc-600 gap-32 justify-center">
+        <div class="colum flex pt-20 px-64 pb-10 text-zinc-600 gap-32 justify-center max-md:pt-16 max-md:px-10 max-md:flex-col max-md:justify-center max-md:gap-10">
             <div class="info_proyect max-w-[80ch]">
                 <div class="info_secondary flex flex-col gap-6 sticky top-32">
                     <div class="text max-w-[60ch] text-[32px] leading-10 text-zinc-600">Tres packaging saludables</div>
-                    <div class="text text-[16px] leading-6 text-zinc-500 font-normal max-w-[64ch]">
+                    <div class="text text-[16px] leading-6 text-zinc-500 font-normal max-w-[64ch] max-md:text-xs">
                         El trabajo finalmente cuenta con 3 packaging a modo de tótem y juego visual y táctil de combinación
                         del espectador y un packaging adaptado a otra estructura
                         diferente de caja para un producto diferente
@@ -126,9 +126,9 @@
                 <img class="image_rect rounded-xl w-[550px]" src="/img/finales3.png" alt="">
             </div>
         </div>
-        <div class="images pt-20 px-64 pb-10 text-zinc-600 justify-center flex flex-col gap-10">
+        <div class="images pt-20 px-64 pb-10 text-zinc-600 justify-center flex flex-col gap-10 max-md:pt-2 max-md:pb-2 max-md:px-10 max-md:flex-col max-md:justify-center max-md:gap-4">
             <div class="text max-w-[60ch] text-[32px] leading-10 text-zinc-600">Captions</div>
-            <div class="text max-w-[60ch] text-[16px] text-zinc-500 font-bold pb-12">Muestra del packaging final con todas
+            <div class="text max-w-[60ch] text-[16px] text-zinc-500 font-bold pb-12 max-md:text-xs">Muestra del packaging final con todas
                 sus caras embolventes viendo como el patrón continua y se va formando por todo el packaging.
                 <br><br> <a class="type text-[#F3536D]">Frambuesa liofilizada</a> - <a
                     class="type text-[#FFB047]">Garzbanzos Spicy</a> - <a class="type text-[#FBCE37]">Snacks de Verduras</a>
@@ -144,42 +144,42 @@
         </div>
 
         
-        <div class="images pt-20 px-64 pb-10 text-white justify-center">
+        <div class="images pt-20 px-64 pb-10 text-white justify-center max-md:pt-2 max-md:pb-2 max-md:px-10 max-md:flex-col max-md:justify-center max-md:gap-4">
             <img src="/img/packruben.png" alt="">
         </div>
 
-        <div class="trabajos flex pt-10 pl-40 pr-40 pb-20 text-zinc-600 flex-col gap-10 justify-center items-center">
-            <div class="titular text-[16px] font-normal flex flex-start items-center">Programas utilizados para este
+        <div class="trabajos flex pt-10 pl-40 pr-40 pb-20 text-zinc-600 flex-col gap-10 justify-center items-center max-md:pt-12 max-md:pb-2 max-md:px-10 max-md:flex-col max-md:justify-center max-md:gap-10">
+            <div class="titular text-[16px] font-normal flex flex-start items-center max-md:text-xs">Programas utilizados para este
                 proyecto</div>
             <div class="programas flex flex-col items-center">
                 <div class="three_programas flex gap-10">
-                    <img class="image_logo w-[40px] rounded-lg opacity-80 hover:opacity-100" src="/img/Procreate.png" alt="">
-                    <img class="image_logo w-[40px] opacity-80 hover:opacity-100" src="/img/Cinema4d.png" alt="">
-                    <img class="image_logo w-[40px] opacity-80 hover:opacity-100" src="/img/Illustratorzinc.svg" alt="">
-                    <img class="image_logo w-[40px] opacity-80 hover:opacity-100" src="/img/InDesignzinc.svg" alt="">
-                    <img class="image_logo w-[40px] opacity-80 hover:opacity-100" src="/img/Photoshopzinc.svg" alt="">
+                    <img class="image_logo w-[40px] rounded-lg opacity-80 hover:opacity-100 max-md:max-w-[30px] max-md:max-h-[29px]" src="/img/Procreate.png" alt="">
+                    <img class="image_logo w-[40px] opacity-80 hover:opacity-100 max-md:max-w-[30px] max-md:max-h-[29px]" src="/img/Cinema4d.png" alt="">
+                    <img class="image_logo w-[40px] opacity-80 hover:opacity-100 max-md:max-w-[30px]" src="/img/Illustratorzinc.svg" alt="">
+                    <img class="image_logo w-[40px] opacity-80 hover:opacity-100 max-md:max-w-[30px]" src="/img/InDesignzinc.svg" alt="">
+                    <img class="image_logo w-[40px] opacity-80 hover:opacity-100 max-md:max-w-[30px]" src="/img/Photoshopzinc.svg" alt="">
                 </div>
             </div>
         </div>
 
-        <div class="links_rrss pt-32 px-80 pb-32 text-zinc-600 justify-center flex flex-row gap-60">
-            <div class="email text-lg flex gap-2 items-center">ruben.maldonado.graphic@gmail.com
+        <div class="links_rrss pt-32 px-80 pb-32 text-zinc-600 justify-center flex flex-row gap-60 max-md:pt-20 max-md:px-10 max-md:flex-col max-md:justify-items-center max-md:gap-20 max-md:flex max-md:justify-center">
+            <div class="email text-lg flex gap-2 items-center max-md:text-[14px] max-md:justify-center">ruben.maldonado.graphic@gmail.com
                     <img class="vector" src="/img/Vector-11zinc.svg">
             </div>
             <div class="links flex flex-col gap-2">
-                <div class="links1 flex gap-32">
-                    <a class="red flex gap-2 items-center" href="https://www.instagram.com/ruben.artes/">Instagram
+                <div class="links1 flex gap-32 max-md:justify-center">
+                    <a class="red flex gap-2 items-center max-md:text-[14px]" href="https://www.instagram.com/ruben.artes/">Instagram
                             <img class="vector" src="/img/Vector-11zinc.svg">
                     </a>
-                    <a class="red flex gap-2 items-center" href="https://www.behance.net/rubenartes">Behance
+                    <a class="red flex gap-2 items-center max-md:text-[14px]" href="https://www.behance.net/rubenartes">Behance
                             <img class="vector" src="/img/Vector-11zinc.svg">
                     </a>
                 </div>
-                <div class="links2 flex gap-32 flex-end">
-                    <a class="red flex gap-2 items-center" href="https://www.instagram.com/ruben.iso">Instagram
+                <div class="links2 flex gap-32 flex-end max-md:justify-center">
+                    <a class="red flex gap-2 items-center max-md:text-[14px]" href="https://www.instagram.com/ruben.iso">Instagram
                             <img class="vector" src="/img/Vector-11zinc.svg">
 
-                    </a> <a class="red flex gap-2 items-center" href="https://www.linkedin.com/in/rubenmaldonado10">LinkedIn
+                    </a> <a class="red flex gap-2 items-center max-md:text-[14px]" href="https://www.linkedin.com/in/rubenmaldonado10">LinkedIn
                             <img class="vector" src="/img/Vector-11zinc.svg">
                     </a>
                 </div>
@@ -187,15 +187,15 @@
 
         </div>
 
-        <div class="foter bg-[#FFE0C5] p-10 flex text-zinc-600 justify-between sticky top-0">
+        <div class="foter bg-[#FFE0C5] p-10 flex text-zinc-600 justify-between sticky top-0 max-md:p-6">
             <div class="foter_image flex flex-start">
                 <NuxtLink to="/">
-                    <img class="vector" src="/img/starzinc.svg">
+                    <img class="vector max-md:max-w-[15px]" src="/img/starzinc.svg">
                 </NuxtLink>
             </div>
-            <div class="email flex gap-2 items-center text-sm">Ruben Maldonado González
+            <div class="email flex gap-2 items-center text-sm max-md:text-xs">Ruben Maldonado González
                 <NuxtLink to="/">
-                    <img class="vector" src="/img/futbolzinc.svg">
+                    <img class="vector max-md:max-w-[15px]" src="/img/futbolzinc.svg">
                 </NuxtLink>
             </div>
         </div>
